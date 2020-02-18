c26a96ebefc8d1f190dc6c5bdac2d0931efa2501
CoderHal <halof.halima@gmail.com>
2020-02-18 16:12:40 +0100

    feat: Create background for icons

---
+++ portfolio/src/atoms/iconbg/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Icon from './style'
+
+const StyleIcon = () => {
+    return (
+        <Icon/>
+    )
+}
+export default StyleIcon
---
+++ portfolio/src/atoms/iconbg/style.js
@@ -0,0 +1,13 @@
+import Styled from 'styled-components'
+import { primary } from '../colors';
+const Icon = Styled.div`
+border: none;
+border-radius: 50%;
+padding: 1rem;
+margin: 1rem;
+width:8rem;
+height:8rem;
+background-color: ${primary};
+`
+
+export default Icon
