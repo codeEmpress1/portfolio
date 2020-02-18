894de86d56dd00c38c25d0ca0ef28d2631061214
CoderHal <halof.halima@gmail.com>
2020-02-18 16:08:37 +0100

    feat: Create Button stories

---
+++ portfolio/src/stories/3-MyButton.stories.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Button from '../atoms/buttons/index'
+
+export default{
+    title: 'Button',
+    // component: Button,
+}
+
+export const FulltimeBtn = () => (
+    <Button>Full Time</Button>
+)
+
+export const ParttimeBtn = () => (
+    <Button>Part Time</Button>
+)
