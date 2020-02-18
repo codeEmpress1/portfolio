a2315884165ce34ae9177da093ace7a916bfba17
CoderHal <halof.halima@gmail.com>
2020-02-18 16:14:52 +0100

    feat: Create story for icon background

---
+++ portfolio/src/stories/2-icon.stories.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import StyledIcon from '../atoms/iconbg'
+
+export default{
+    
+    title: 'Icons',
+    component: StyledIcon,
+}
+
+export const TextIcon = () => (
+    <StyledIcon/>
+)
