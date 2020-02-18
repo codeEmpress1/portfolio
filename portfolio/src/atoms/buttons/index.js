3834bffea9686070d05eea0b8a2f10e7b74fecfb
CoderHal <halof.halima@gmail.com>
2020-02-18 16:06:22 +0100

    feat: Create buttons

---
+++ portfolio/src/atoms/buttons/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import StyledButton from './style'
+// import Button from '@material-ui/core/Button'
+
+const MyButton = (props) => <StyledButton>{props.children}</StyledButton>
+
+export default MyButton
---
+++ portfolio/src/atoms/buttons/style.js
@@ -0,0 +1,14 @@
+// import React from 'react'
+import Styled from 'styled-components'
+// import Button from '@material-ui/core/Button'
+import { primaryDark } from '../colors'
+const StyledButton = Styled('Button')`
+padding: .5rem 1rem;
+border-radius: 1rem;
+background-color: ${primaryDark};
+// color: ${primaryDark};
+font-weight: bold;
+color
+`
+
+export default StyledButton
